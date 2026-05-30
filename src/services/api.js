29cf014a2c27928ce28dceb2f4a251415d830c68
Rajudotin <balavenkatarajusingampalli@gmail.com>
2026-05-30 14:38:56 +0530

    Connected frontend to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.1.37:5000/api",
+  baseURL: "https://task-manager-react-native-app.onrender.com/api",
 });
 
 export default API;
